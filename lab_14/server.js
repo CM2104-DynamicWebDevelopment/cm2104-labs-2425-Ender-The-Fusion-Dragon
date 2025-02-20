647710170aa262b06f257826d261c47356f1032f
Callum Friel <enderassassino@gmail.com>
2025-02-20 11:54:10 +0000

    added more code to ndex.ejs and erver.js

--- lab_14/server.js
+++ lab_14/server.js
@@ -8,9 +8,24 @@ app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
-// index page
+// // index page
+//app.get('/', function(req, res){
+//    res.render('pages/index');
+//});
+
+// index pge
 app.get('/', function(req, res){
-    res.render('pages/index');
+    var drinks = [
+        {name: 'Bloody May', drunkness: 3},
+        {name: 'Martini', drunkness: 5},
+        {name: 'Scotch', drunkness: 10}
+    ];
+    var tagline = "Any coe of your own tht you haven't looked t for six or more months might as well have been written by omeone else.";
+
+    res.render('pages/index', {
+        drnks: drinks,
+        tagline: tagline
+    });
 });
 
 // about page
