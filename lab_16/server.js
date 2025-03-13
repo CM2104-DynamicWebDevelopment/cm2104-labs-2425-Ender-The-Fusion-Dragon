27e94ab3423b3189e13ba0461ddef03431910459
Callum Friel <enderassassino@gmail.com>
2025-03-13 11:50:56 +0000

    fixed error

--- lab_16/server.js
+++ lab_16/server.js
@@ -19,7 +19,7 @@ io.on('connection', function(socket){
     });
 
     socket.on('chat message', function(msg){
-        console.emit('message: ' + msg);
+        io.emit('message: ' + msg);
     });
 });
 
