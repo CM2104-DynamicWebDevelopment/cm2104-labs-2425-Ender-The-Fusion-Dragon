5da55e3ec0d08f29e35b77b60c926e9487963838
Callum Friel <enderassassino@gmail.com>
2025-03-13 11:08:48 +0000

    added more code to main.js

--- lab_16/public/scripts/main.js
+++ lab_16/public/scripts/main.js
@@ -1,1 +1,22 @@
 var socket = io();
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('pages/index');
+});
+
+io.on('connection', function(socket){
+    console.log('A user connected');
+});
+
+http.listen(8080, funtion(){
+    console.log('listening on *: 8080');
+});
