613faedb84b9785286930437090ba0301917698d
Callum Friel <enderassassino@gmail.com>
2025-03-27 11:13:05 +0000

    updated lab_5 server.js with error handling

--- lab_15/server.js
+++ lab_15/server.js
@@ -14,13 +14,14 @@ const dbname = 'profiles'; // the data base we want to access
 
 const express = require('express'); //npm install express
 const session = require('express-session'); //npm install express-session
-const bodyParser = require('body-parser'); //npm install body-parser
-
+const bodyParser = require('body-parser'); //npm install body-parse
 const app = express();
 
 
 //this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
-app.use(session({ secret: 'example' }));
+app.use(session({ 
+  secret: 'example' 
+}));
 
 //code to define the public "static" folder
 app.use(express.static('public'))
@@ -36,36 +37,50 @@ app.set('view engine', 'ejs');
 //variable to hold our Database
 var db;
 
-//run the connect method.
-connectDB();
-//this is our connection to the mongo db, ts sets the variable db as our database
-async function connectDB() {
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log('Connected successfully to server');
-    db = client.db(dbname);
-    //everything is good lets start
-    app.listen(8080);
-    console.log('Listening for connections on port 8080');
-}
+MongoClient.connect(url, function(err, database){
+  if(!err){
+    db = database;
+  }else{
+    console.log("Error connecting to databse: " + err);
+  }
+
+  //everything is good lets start
+  app.listen(8080);
+  console.log('Listening for connections on port 8080');
+});
 
 
 //********** GET ROUTES - Deal with displaying pages ***************************
 
 //this is our root route
 app.get('/', function(req, res) {
-  //if the user is not logged in redirect them to the login page
-  if(!req.session.loggedin){res.redirect('/login');return;}
+  if(!db){
+    res.render('pages/error');
+    return;
+  }
+
+  if(!req.session.loggedin){
+    res.redirect('/login');
+    return;
+  }
 
   //otherwise perfrom a search to return all the documents in the people collection
-  db.collection('people').find().toArray(function(err, result) {
-    if (err) throw err;
-    //the result of the query is sent to the users page as the "users" array
-    res.render('pages/users', {
-      users: result
-    })
-  });
+  db.collection('people').find().toArray(function (err, result) {
+    //if there is an error doing the user search
+    if(err){
+      //render the bad error page and passdown the error
+      res.render('pages/baderror',{error:err} );
+      return;
+    }
 
+    var currentuser = req.session.currentuser;
+    db.collection('people').findOne({"login.username": currentuser}, function(err, userresult){
+      res.render('pages/users', {
+        users: result,
+        user: userresult
+      })
+    });
+  });
 });
 
 //this is our login route, all it does is render the login.ejs page.
@@ -75,33 +90,47 @@ app.get('/login', function(req, res) {
 
 
 app.get('/profile', function(req, res) {
-  if(!req.session.loggedin){res.redirect('/login');return;}
-  
+  if(!req.session.loggedin){
+    res.redirect('/login');
+    return;
+  }
   
   var uname = req.query.username;
   
- 
-  db.collection('people').findOne({"login.username": uname}, function(err, result) {
-    if (err) throw err;
+  db.collection('people').findOne({
+    "login.username": uname
+  }, function(err, result) {
+    if(err){
+      res.render('pages/baderror', {error:err});
+      return;
+    }
    
-
-
     res.render('pages/profile', {
       user: result
     })
   });
 
 });
+
 //adduser route simply draws our adduser page
 app.get('/adduser', function(req, res) {
-  if(!req.session.loggedin){res.redirect('/login');return;}
+
+  if(!req.session.loggedin){
+    res.redirect('/login');
+    return;
+  }
   res.render('pages/adduser')
 });
+
 //remuser route simply draws our remuser page
 app.get('/remuser', function(req, res) {
-  if(!req.session.loggedin){res.redirect('/login');return;}
+  if(!req.session.loggedin){
+    res.redirect('/login');
+    return;
+  }
   res.render('pages/remuser')
 });
+
 //logour route cause the page to Logout.
 //it sets our session.loggedin to false and then redirects the user to the login
 app.get('/logout', function(req, res) {
@@ -126,31 +155,49 @@ app.post('/dologin', function(req, res) {
 
 
   db.collection('people').findOne({"login.username":uname}, function(err, result) {
-    if (err) throw err;
+    if (err){
+      res.render('pages/baderror', {error:err});
+      return;
+    }
 
 
-    if(!result){res.redirect('/login');return}
+    if(!result){
+      res.redirect('/login');
+      return;
+    }
 
+    if(result.login.password == pword){ 
+      req.session.loggedin = true;
+      req.session.currentuser = uname;
+      res.redirect('/')
 
-
-    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
-
-
-
-    else{res.redirect('/login')}
+    }else{
+      res.redirect('/login')
+    }
   });
 });
 
 //the delete route deals with user deletion based on entering a username
 app.post('/delete', function(req, res) {
   //check we are logged in.
-  if(!req.session.loggedin){res.redirect('/login');return;}
+  if(!req.session.loggedin){
+    res.redirect('/login');
+    return;
+  }
+
   //if so get the username variable
   var uname = req.body.username;
 
   //check for the username added in the form, if one exists then you can delete that doccument
-  db.collection('people').deleteOne({"login.username":uname}, function(err, result) {
-    if (err) throw err;
+  db.collection('people').deleteOne({
+    "login.username":uname
+  }, function(err, result) {
+
+    if (err){
+      res.render('pages/baderror', {error:err});
+      return;
+    }
+
     //when complete redirect to the index
     res.redirect('/');
   });
@@ -172,24 +219,52 @@ app.post('/delete', function(req, res) {
 
 app.post('/adduser', function(req, res) {
   //check we are logged in
-  if(!req.session.loggedin){res.redirect('/login');return;}
+  if(!req.session.loggedin){
+    res.redirect('/login');
+    return;
+  }
 
   //we create the data string from the form components that have been passed in
 
 var datatostore = {
 "gender":req.body.gender,
-"name":{"title":req.body.title,"first":req.body.first,"last":req.body.last},
-"location":{"street":req.body.street,"city":req.body.city,"state":req.body.state,"postcode":req.body.postcode},
+"name":{
+  "title":req.body.title,
+  "first":req.body.first,
+  "last":req.body.last
+},
+
+"location":{
+  "street":req.body.street,
+  "city":req.body.city,
+  "state":req.body.state,
+  "postcode":req.body.postcode
+},
+
 "email":req.body.email,
-"login":{"username":req.body.username,"password":req.body.password},
-"dob":req.body.dob,"registered":Date(),
-"picture":{"large":req.body.large,"medium":req.body.medium,"thumbnail":req.body.thumbnail},
-"nat":req.body.nat}
+"login":{
+  "username":req.body.username,
+  "password":req.body.password
+},
+
+"dob":req.body.dob,
+"registered":Date(),
+"picture":{
+  "large":req.body.large,
+  "medium":req.body.medium,
+  "thumbnail":req.body.thumbnail
+},
+
+"nat":req.body.nat
+}
 
 
 //once created we just run the data string against the database and all our new data will be saved/
   db.collection('people').insertOne(datatostore, function(err, result) {
-    if (err) throw err;
+    if (err){
+      res.render('pages/baderror', {error:err});
+      return;
+    }
     console.log('saved to database')
     //when complete redirect to the index
     res.redirect('/')
